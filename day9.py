ba3a3d8d244de0076b9778ae00ad71ee55cd6b38
Neheloxin <Nehanis@outlook.com>
2026-06-02 17:23:08 +0530

    day9: added total_amount column, saved to results.csv

---
+++ day9.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.read_csv("accounts.csv")
+print(df)
+
+def compound_interest(principal , rate , time):
+    ci = principal * (1 + rate/100) ** time
+    return ci
+
+for index , account in df.iterrows():
+    ci = compound_interest(account["principal"] , account["rate"] , account["time"])
+    print(f"Details Of Account Holder : {account['name']} , Initial Amount : {account['principal']} , Compound Interest : {round(ci - account['principal'],2)} , Total Amount : {round(ci,2)}")
+
+df["total_amount"] = round(df["principal"] * (1 + df["rate"]/100) ** df["time"],2)
+df.to_csv("results.csv", index=False)
